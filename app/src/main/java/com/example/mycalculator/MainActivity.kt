07c2f0991cd82f6c509e0a6055ff6944e4614915
isa <isa@isa-MacBook-Air.local>
2022-01-01 22:19:02 +0300

    [#3]feat: add math func

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -3,15 +3,50 @@ package com.example.mycalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var isLastNumeric: Boolean = false
+    var isLastDoc = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun onDigit(view: View) {
-        Toast.makeText(this, "Button works fine", Toast.LENGTH_SHORT).show()
+        tv_input.append((view as Button).text)
+        isLastNumeric = true
+    }
+
+    fun onClear(view: View){
+        tv_input.text=""
+        isLastNumeric = false
+        isLastDoc = false
+    }
+
+    fun onDecimalPoint(view: View){
+        if(isLastNumeric && !isLastDoc){
+            tv_input.append(".")
+            isLastNumeric = false
+            isLastDoc = true
+        }
+    }
+
+    private fun isOperatorAdded(value: String): Boolean{
+        return if(value.startsWith("-"))
+            false
+        else
+            value.contains("/") || value.contains("*") || value.contains("+") || value.contains("-")
+    }
+
+    fun onOperator(view: View){
+        if(isLastNumeric || !isOperatorAdded(tv_input.text.toString())){
+            tv_input.append((view as Button).text)
+            isLastNumeric = false
+            isLastDoc = false
+        }
     }
 }
