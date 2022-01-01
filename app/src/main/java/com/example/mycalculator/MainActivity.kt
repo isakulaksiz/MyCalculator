031dd1e335b7b0c2c6910950a4acbbd4cd2fb3d3
isa <isa@isa-MacBook-Air.local>
2022-01-01 22:47:09 +0300

    [#3]fix: check subtraction method

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
     var isLastNumeric: Boolean = false
@@ -47,6 +48,32 @@ class MainActivity : AppCompatActivity() {
             tv_input.append((view as Button).text)
             isLastNumeric = false
             isLastDoc = false
+        }
+    }
+
+    fun onEqual(view: View){
+        lateinit var tvValue:String
+        var prefix = ""
+        if(isLastNumeric)
+            tvValue = tv_input.text.toString()
+        try {
+                if(tvValue.startsWith("-")){
+                    prefix = "-"
+                    tvValue = tvValue.substring(1) // -215 önündeki eksi işaretine göre kontrol edildi
+                }
+                if(tvValue.contains("-")){
+                    val splitValue = tvValue.split("-")
+                    var firstParam = splitValue[0]
+                    var secondParam = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        firstParam = prefix + firstParam
+                    }
+                    tv_input.text = (firstParam.toDouble() - secondParam.toDouble()).toString()
+                }
+
+        }catch (e: ArithmeticException){
+            e.printStackTrace()
         }
     }
 }
